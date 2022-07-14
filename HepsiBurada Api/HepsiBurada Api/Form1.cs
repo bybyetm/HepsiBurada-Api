e9d03e47bf572f0787205269a5889b8ad35da425
DOĞU KEMAL ŞEN <105115254+bybyetm@users.noreply.github.com>
2022-07-14 19:20:59 +0300

    Add files via upload

---
+++ HepsiBurada Api/HepsiBurada Api/Form1.cs
@@ -0,0 +1,140 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HepsiBurada_Api
+{
+    public partial class Form1 : Form
+    {
+        string username = "xx";
+        string password = "xx";
+        string merchantId = "xx";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IRestResponse Urunlistegetir(string username, string password, string merchantId)
+        {
+            var client = new RestClient("https://listing-external-sit.hepsiburada.com/listings/merchantid/" + merchantId);
+            string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username + ":" + password));
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("merchantid", merchantId);
+            request.AddHeader("content-type", "application/xml");
+            request.AddHeader("Authorization", "Basic " + svcCredentials);
+            IRestResponse response = client.Execute(request) as RestResponse;
+            return response;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UrunListeGoster();
+        }
+
+     //   Şimdi Urunlistegetir Yordamımızı Çağıralım
+
+        void UrunListeGoster()
+        {
+           
+
+            IRestResponse urunListe = Urunlistegetir(username, password, merchantId);
+            //Durum Kodu 'Ok' ise
+            if (urunListe.StatusCode == HttpStatusCode.OK)
+            {
+                MessageBox.Show(urunListe.Content);
+            }
+            //Durum Kodu 'Ok' değilse
+            else
+            {
+                MessageBox.Show(urunListe.StatusCode.ToString());
+            }
+        }
+
+        /*   Ürünlerin Güncellenmesi
+
+           Hepsiburada üzerinde ürünlerin fiyat, stok adedi, bir defada maksimum satınalma miktarı gibi alanlar güncellenebiliyor.
+
+           Bu güncellemeyi aşağıda gösterilen string üzerinde yapıyoruz.Daha sonra upload yapıyoruz.
+
+           stringimiz
+        */
+        // Birden fazla listing var ise ister <listing> </listing> arasına yazıp for ile göndürürsünüz isterse alt alta yazabilirsiniz 
+
+        string strXml = @"<listings>
+      
+         <listing>
+        <UniqueIdentifier></UniqueIdentifier>
+        <HepsiburadaSku>ZYHPCOCACCOL055</HepsiburadaSku>
+        <MerchantSku>COLA123</MerchantSku>
+        <ProductName>Coca Cola Cam 200 Ml</ProductName>
+        <Price>6000</Price>
+        <AvailableStock>100</AvailableStock>
+        <DispatchTime>1</DispatchTime>
+        <CargoCompany1>Yurtiçi Kargo</CargoCompany1>
+        <CargoCompany2>Aras Kargo</CargoCompany2>
+        <CargoCompany3>Horoz Lojistik</CargoCompany3>
+        <MaximumPurchasableQuantity>4</MaximumPurchasableQuantity>
+        </listing>
+    
+        <listing>
+        <UniqueIdentifier></UniqueIdentifier>
+        <HepsiburadaSku>ZYHPCOCACCOL055</HepsiburadaSku>
+        <MerchantSku>COLA123</MerchantSku>
+        <ProductName>Coca Cola Cam 200 Ml</ProductName>
+        <Price>6000</Price>
+        <AvailableStock>100</AvailableStock>
+        <DispatchTime>1</DispatchTime>
+        <CargoCompany1>Yurtiçi Kargo</CargoCompany1>
+        <CargoCompany2>Aras Kargo</CargoCompany2>
+        <CargoCompany3>Horoz Lojistik</CargoCompany3>
+        <MaximumPurchasableQuantity>4</MaximumPurchasableQuantity>
+        </listing>
+    
+
+    </listings>";
+
+
+            string UrunGuncelle(string username, string password, string merchantId, string strXml)
+            {
+                var uri = "https://listing-external-sit.hepsiburada.com/listings/merchantid/" + merchantId + "/inventory-uploads";
+                var client = new RestClient(uri);
+                var request = new RestRequest(Method.POST);
+                string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username + ":" + password));
+
+                request.AddHeader("merchantid", merchantId);
+                request.AddHeader("Authorization", "Basic " + svcCredentials);
+
+                request.RequestFormat = RestSharp.DataFormat.Xml;
+
+                if (!string.IsNullOrEmpty(strXml))
+                    request.AddParameter("application/xml", strXml, ParameterType.RequestBody);
+
+                var result = "";
+                var response = client.Execute(request);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    result = response.Content;
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    result = response.StatusCode.ToString();
+                }
+                return result;
+
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             UrunGuncelle( username,  password,  merchantId,  strXml);
+        }
+    }
+}
---
+++ HepsiBurada Api/HepsiBurada Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("HepsiBurada Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HepsiBurada Api")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5e5bb23a-8b11-4c37-83f1-fd6e788a0bb6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
